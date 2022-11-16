0feee8b26697ae2a42def2194384149b48bb5cc4
dmuladil <dmuladil@iu.edu>
2022-11-15 21:34:01 -0500

    commit

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from aquarium import Aquarium
 from fish import Fish
-
+import random
 if __name__ == '__main__':
     print("Running Main")
+    origin = (10, 10)  # starting point for the training fish
     fishes = []
-    origin = (0,5)
+    possibleX = [0, 2, 4, 6, 8, 12, 14, 16, 18, 20]
+    possibleY = [0, 2, 4, 6, 8, 12, 14, 16, 18, 20]
+    for i in range(0, 10):
+        x = random.randint(0, 9)
+        y = random.randint(0, 9)
+        loc = (possibleX[x], possibleY[y])
+        f = Fish.randomFishGenerator(loc)
+        fishes.append(f)
+    aquarium1 = Aquarium(20, fishes)
+
+    training_fishes = [Fish.randomFishGenerator(origin) for i in range(10)]
+
     print("Run Demo:\n")
-    print("Fish origin: ",(0,5))
-
-    f = Fish(origin, 5, 1, 0)
-    fishes.append(f)
-    aquarium = Aquarium(6, fishes)
-
-    for i in range(1, 10):
-        f = Fish.randomFishGenerator(origin)
-        print(str(f.vision) + ", " + str(f.speed) + ", " + str(f.riskAwareness))
+    print("Fish origin: ", (10, 10))
 
 
-    
+
