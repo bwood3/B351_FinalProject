6354b54b25276406f1e241a27453da59d42b0ce7
dmuladil <dmuladil@iu.edu>
2022-11-15 19:01:38 -0500

    commit

--- fish.py
+++ fish.py
@@ -1,12 +1,14 @@
 # todo because aquarium will be initializing fish should we have "attribute randomizer with limit" method in that class
 # todo instead of timer we could say a fish with a higher speed gets x more moves to every 1 move of a slower fish?
-class Fish:
+import random
 
+
+class Fish:
     # constant variables that will not change
-    #for termination state
+    # for termination state
     ALIVE = 1
     DEAD = 0
-    #for movements (row, col)
+    # for movements (row, col)
     UP = (0, -1)
     DOWN = (0, 1)
     RIGHT = (1, 0)
@@ -15,27 +17,28 @@ class Fish:
     NORTHWEST = tuple(map(sum, zip(UP, LEFT)))
     SOUTHEAST = tuple(map(sum, zip(DOWN, RIGHT)))
     SOUTHWEST = tuple(map(sum, zip(DOWN, LEFT)))
+    POINTS = 15
 
     # all fish will have various attributes (initialized by aquarium class)
     # if we are using our training fish, movement pattern will be defined by A*
     # if we are not using training fish, it will be pre-defined as argument
-    def __init__(self, loc, vision, speed, riskTolerance, movementPattern = None):
+    def __init__(self, loc, vision, speed, riskAwareness, movementPattern=None):
         self.loc = loc
         self.vision = vision
         self.speed = speed
-        self.riskTolerance = riskTolerance
+        self.riskAwareness = riskAwareness
         self.status = self.ALIVE
         self.score = 0
 
-        if(movementPattern == None):
+        if (movementPattern == None):
             self.movementQueue = self.path()
         else:
             self.movementQueue = movementPattern
 
-    #in our aquarium we will have a loop that calls this method for every fish present (held in stack) to update their location
+    # in our aquarium we will have a loop that calls this method for every fish present (held in stack) to update their location
     def getMove(self, visionGrid):
 
-        #todo add an interpretation for visionGrid
+        # todo add an interpretation for visionGrid
         # check if current grid interferes with current path (present in movementQueue)
 
         if (len(self.movementQueue) > 0):
@@ -47,9 +50,10 @@ class Fish:
         print("Fish Destination: ", tuple(map(sum, zip(curLoc, delta))))
         return tuple(map(sum, zip(curLoc, delta)))
 
-    #A list movements for a path training fish will take (A* for training fish, pre-defined for non-training fish)
+    # A list movements for a path training fish will take (A* for training fish, pre-defined for non-training fish)
     def path(self):
-        return [self.UP, self.RIGHT, self.DOWN, self.NORTHEAST, self.NORTHEAST, self.SOUTHEAST, self.RIGHT, self.RIGHT] # demo code
+        return [self.UP, self.RIGHT, self.DOWN, self.NORTHEAST, self.NORTHEAST, self.SOUTHEAST, self.RIGHT,
+                self.RIGHT]  # demo code
 
     # move direction is a tuple
     def myDest(self, moveDirection):
@@ -76,6 +80,14 @@ class Fish:
             quit()
 
         return self.translateMove(self.loc, delta)
+
+    def randomFishGenerator(self, loc):
+        self.loc = loc
+        self.vision = random.randint(1, self.POINTS)
+        self.POINTS -= self.vision
+        self.speed = random.randint(1, self.POINTS)
+        self.POINTS -= self.speed
+        self.riskAwareness = self.POINTS
 
     def __repr__(self):
         return "fish"
